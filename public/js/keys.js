1d5f719c64c6010258fe204bf6619582cb1f7d56
Hector Fuentes <hgfuentes@falabella.cl>
2023-04-24 15:05:15 -0400

    feat: db conn to rds aws

--- public/js/keys.js
+++ public/js/keys.js
@@ -1,11 +1,12 @@
 module.exports = {
 
     database: {
-        connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'arbim'
+        connectionLimit: process.env.RDS_CONN_LIMIT || 10,
+        host: process.env.RDS_HOSTNAME || 'localhost',
+        port: process.env.RDS_PORT || 3306,
+        user: process.env.RDS_USERNAME || 'root',
+        password: process.env.RDS_PASSWORD || '',
+        database: process.env.RDS_DB_NAME || 'arbim'
     }
 
 };
