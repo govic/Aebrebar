00f542fa2eb1c85f1c419262c36bed2f463b7b54
Hector Fuentes <hgfuentes@falabella.cl>
2023-04-25 16:03:18 -0400

    chore: db config

--- public/js/keys.js
+++ public/js/keys.js
@@ -1,12 +1,12 @@
 module.exports = {
 
     database: {
-        connectionLimit: process.env.RDS_CONN_LIMIT || 10,
+        connectionLimit: 10,
         host: process.env.RDS_HOSTNAME || 'localhost',
         port: process.env.RDS_PORT || 3306,
         user: process.env.RDS_USERNAME || 'root',
         password: process.env.RDS_PASSWORD || '',
-        database: process.env.RDS_DB_NAME || 'arbim'
+        database: 'arbim'
     }
 
 };
